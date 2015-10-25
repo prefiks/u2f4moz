a5fc95dfe36b4a4dc3e7b7297a714db72f4bb9b2
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2015-10-25 10:39:34 -0700

    Setup ESLint and fix issues found

--- ext/index.js
+++ ext/index.js
@@ -1,35 +1,39 @@
+/*global self:true*/
+"use strict";
+
 var self = require("sdk/self");
 var pageMod = require("sdk/page-mod");
-var child_process = require("sdk/system/child_process");
-var { emit } = require('sdk/event/core');
+var childProcess = require("sdk/system/child_process");
+var { emit } = require("sdk/event/core");
 var system = require("sdk/system");
 var url = require("sdk/url");
 
 function execBin(args, stdin, event, callbackid, worker) {
-  console.info("EB1",args,event);
-  var exe = system.platform+"_"+system.architecture+"-"+system.compiler+"/u2f"+
+  console.info("EB1", args, event);
+  var exe = system.platform + "_" + system.architecture + "-" + system.compiler + "/u2f" +
     (system.platform == "winnt" ? ".exe" : "");
-  var path = url.toFilename(self.data.url("../bin/"+exe));
-  console.info("EB2",path);
-  var cmd = child_process.spawn(path, args, {});
-  console.info("EB3",cmd);
-  var response = {value:""};
+  var path = url.toFilename(self.data.url("../bin/" + exe));
+  console.info("EB2", path);
+  var cmd = childProcess.spawn(path, args, {});
+  console.info("EB3", cmd);
+  var response = {value: ""};
   cmd.stdout.on("data", function(data) {
-    console.info("EBD",data);
+    console.info("EBD", data);
     response.value += data;
-  })
+  });
   cmd.on("close", function(code) {
-    console.info("EBC",code,response.value.length);
-    if (code == 0 && response.value.length)
+    console.info("EBC", code, response.value.length);
+    if (code == 0 && response.value.length) {
       worker.port.emit(event, callbackid, JSON.parse(response.value));
-    else
+    } else {
       worker.port.emit(event, callbackid, {errorCode: 1});
-  })
+    }
+  });
   emit(cmd.stdin, "data", stdin);
   emit(cmd.stdin, "end");
 }
 
-pageMod.PageMod({
+pageMod.PageMod({ // eslint-disable-line new-cap
   include: "*",
   contentScriptWhen: "start",
   attachTo: ["existing", "top"],
@@ -40,11 +44,11 @@ pageMod.PageMod({
       var req = Array.isArray(requests) ? requests[0] : requests;
       execBin(["r", domain], JSON.stringify(req),
        "registerResponse", callbackid, worker);
-    })
+    });
     worker.port.on("sign", function(signRequests, callbackid, domain) {
       var req = Array.isArray(signRequests) ? signRequests[0] : signRequests;
       execBin(["s", domain], JSON.stringify(req),
        "signResponse", callbackid, worker);
-    })
+    });
   }
 });
