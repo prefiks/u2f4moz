1c96422c5252db4658f8b21964c7e9fc2eac21dc
Paweł Chmielowski <prefiks@prefiks.org>
2015-10-31 11:53:11 +0100

    Re-enable including config.h

--- c_src/libu2f-host/cencode.c
+++ c_src/libu2f-host/cencode.c
@@ -15,7 +15,7 @@
  along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
-//#include <config.h>
+#include <config.h>
 
 /*
 cencoder.c - c source to a base64 encoding algorithm implementation
--- c_src/libu2f-host/error.c
+++ c_src/libu2f-host/error.c
@@ -15,7 +15,7 @@
   along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
-//#include <config.h>
+#include <config.h>
 #include <u2f-host.h>
 
 #define ERR(name, desc) { name, #name, desc }
