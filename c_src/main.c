d11a90c7e5f1b1f1c969ddc5d38cc4adb8063407
Paweł Chmielowski <prefiks@prefiks.org>
2015-11-14 16:05:01 +0100

    Rename input_len_buf to input_len

--- c_src/main.c
+++ c_src/main.c
@@ -73,26 +73,26 @@ read_action(int timeout) {
     return &eof_op;
 
   if (op == 'r' || op == 's') {
-    char input_len_buf[9];
+    char input_buf[9];
     long challenges_lengths[16];
     long challenges_num, challenges_buf_len = 0, domain_len;
     int i;
 
-    if (!read_n_bytes(input_len_buf, 8))
+    if (!read_n_bytes(input_buf, 8))
       return &eof_op;
 
-    input_len_buf[8] = '\0';
-    challenges_num = strtol(input_len_buf + 4, NULL, 16);
-    input_len_buf[4] = '\0';
-    domain_len = strtol(input_len_buf, NULL, 16);
+    input_buf[8] = '\0';
+    challenges_num = strtol(input_buf + 4, NULL, 16);
+    input_buf[4] = '\0';
+    domain_len = strtol(input_buf, NULL, 16);
 
     if (challenges_num >= sizeof(challenges_lengths)/sizeof(challenges_lengths[0]))
       return &eof_op;
 
     for (i = 0; i < challenges_num; i++) {
-      if (!read_n_bytes(input_len_buf, 4))
+      if (!read_n_bytes(input_buf, 4))
         return &eof_op;
-      challenges_lengths[i] = strtol(input_len_buf, NULL, 16);
+      challenges_lengths[i] = strtol(input_buf, NULL, 16);
       challenges_buf_len += challenges_lengths[i];
     }
 
