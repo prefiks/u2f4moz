6027dd1d871c244ea487fdf8549e14c45290218c
Paweł Chmielowski <prefiks@prefiks.org>
2017-02-09 19:54:37 +0100

    Add ability to hide send/received data in displayed logs

--- ext/data/logs.js
+++ ext/data/logs.js
@@ -1,9 +1,31 @@
 /* global self, document */
-self.port.on("logs", function(logs) {
+var receivedLogs;
+
+function displayLogs() {
+  if (!receivedLogs)
+    return;
+
   var ul = document.getElementsByTagName("ul")[0];
-  logs.forEach(v => {
+  var hd = document.getElementById("hide-data").checked;
+
+  while (ul.firstChild)
+    ul.removeChild(ul.firstChild);
+
+  receivedLogs.forEach(v => {
     var el = document.createElement("li");
+    if (hd)
+      v = v.replace(/("?challenge"?)\s*:\s*".*?"/g, "$1:\"...\"").
+        replace(/("?keyHandle"?)\s*:\s*".*?"/g, "$1:\"...\"").
+        replace(/("?signatureData"?)\s*:\s*".*?"/g, "$1:\"...\"").
+        replace(/("?clientData"?)\s*:\s*".*?"/g, "$1:\"...\"");
     el.textContent = v;
     ul.appendChild(el);
   });
+}
+
+self.port.on("logs", function(logs) {
+  receivedLogs = logs;
+  displayLogs();
 });
+
+document.getElementById("hide-data").addEventListener("input", displayLogs, false);
